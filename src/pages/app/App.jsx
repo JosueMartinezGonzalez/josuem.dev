3d48958afb8ae5419d67684987a4658bfe417aa6
Josué Martinez <josuearmandomartinez@gmail.com>
2022-04-21 14:27:53 -0500

    Se inicializó el proyecto, se instalaron las dependencias de Webpack, Babel, React, plugins-loaders de Css y HTML Se realizo la configuración de babel, webpack y plugins Se organizaron las carpetas para los components y pages dentro del src

---
+++ src/pages/app/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './global.css'
+
+const App = () => {
+    return (
+        <h1>Hola mundo! Soy Josue Martinez</h1>
+    )
+}
+
+export {App}
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/pages/app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new miniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3005,
+        open: true,
+    }
+}
