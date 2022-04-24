c40fe302407978ead225849338ec50106a621fc0
Josué Martinez <josuearmandomartinez@gmail.com>
2022-04-23 22:27:26 -0500

    Se creó el contenido de Footer y se le aplicaron estilos

---
+++ src/components/components portfolio/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Footer.css'
+
+function Footer() {
+    return(
+        <footer>
+            <h3>Josue Armando Martinez Gonzalez @devjamg | © Todos los derechos reservados - 2022.</h3>
+            <p>“Programa lo que puedas imaginar y guarda antes de cerrar” 💚</p>
+        </footer>
+    )
+}
+
+export { Footer }
--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 import { Header } from '../../components/components portfolio/Header/index'
 import { Main } from '../../components/components portfolio/Main/index'
-// import { Footer } from '../../components/components portfolio/Footer/index'
+import { Footer } from '../../components/components portfolio/Footer/index'
 
 function Portfolio() {
     return(
         <div style={{background: "linear-gradient(135deg, var(--red) 0%, var(--purple) 50%, var(--dark-green) 100%)"}}>
             <Header />
             <Main />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
